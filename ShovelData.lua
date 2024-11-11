c165fa14e6d49342a6f605304510dc9675bc7ccc
LordHippo <146130238+ReimuSimpu@users.noreply.github.com>
2024-11-10 23:04:03 -0600

    Create ShovelData.lua

---
+++ ShovelData.lua
@@ -0,0 +1,16 @@
+local SHOVEL_DATA = {
+    ['Flimsy Shovel'] = { Next_Shovel = 'Normal Shovel', Cost_Of_Next = 2000, Rank_Current_Shovel = 1, Icon = 'rbxassetid://16039332097', ColorText = Color3.new(1, 1, 1) },
+    ['Normal Shovel'] = { Next_Shovel = 'Bluesteel Shovel', Cost_Of_Next = 12500, Rank_Current_Shovel = 2, Icon = 'rbxassetid://16039331991', ColorText = Color3.new(1, 1, 1) },
+    ['Bluesteel Shovel'] = { Next_Shovel = 'Sharp Shovel', Cost_Of_Next = 55000, Rank_Current_Shovel = 3, Icon = 'rbxassetid://16039333077', ColorText = Color3.new(1, 1, 1) },
+    ['Sharp Shovel'] = { Next_Shovel = 'Pro Shovel', Cost_Of_Next = 250000, Rank_Current_Shovel = 4, Icon = 'rbxassetid://16039332182', ColorText = Color3.new(1, 1, 1) },
+    ['Pro Shovel'] = { Next_Shovel = 'Platinum Shovel', Cost_Of_Next = 250000, Rank_Current_Shovel = 5, Icon = 'rbxassetid://16039332500', ColorText = Color3.new(1, 1, 1) },
+    ['Golden Shovel'] = { Next_Shovel = 'Platinum Shovel', Cost_Of_Next = 250000, Rank_Current_Shovel = 6, Icon = 'rbxassetid://16039332602', ColorText = Color3.new(1, 1, 1) },
+
+    ['Platinum Shovel'] = { Next_Shovel = 'Emerald Shovel', Cost_Of_Next = 1250000, Rank_Current_Shovel = 7, Icon = 'rbxassetid://16039332375', ColorText = Color3.new(1, 1, 1) },
+    ['Emerald Shovel'] = { Next_Shovel = 'Sapphire Shovel', Cost_Of_Next = 4000000, Rank_Current_Shovel = 8, Icon = 'rbxassetid://16039332703', ColorText = Color3.new(1, 1, 1) },
+    ['Sapphire Shovel'] = { Next_Shovel = 'Amethyst Shovel', Cost_Of_Next = 12500000, Rank_Current_Shovel = 9, Icon = 'rbxassetid://16039332265', ColorText = Color3.new(1, 1, 1) },
+    ['Amethyst Shovel'] = { Next_Shovel = 'NO NEXT SHOVEL', Cost_Of_Next = math.huge, Rank_Current_Shovel = 10, Icon = 'rbxassetid://16039332930', ColorText = Color3.new(1, 1, 1) },
+    ['Diamond Shovel'] = { Next_Shovel = 'NO NEXT SHOVEL', Cost_Of_Next = math.huge, Rank_Current_Shovel = 11, Icon = 'rbxassetid://16039332788', ColorText = Color3.new(1, 1, 1) },
+}
+
+return SHOVEL_DATA
