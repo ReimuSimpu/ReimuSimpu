6c3b93fa15f090ad29bde16077b36b5a5729a7dd
LordHippo <146130238+ReimuSimpu@users.noreply.github.com>
2025-02-10 08:36:37 -0600

    Update la.lua

--- la.lua
+++ la.lua
@@ -1,17 +1,27 @@
-local mod = {}
+local LocalPlayer = game.Players.LocalPlayer
 
-mod.DestroyChildren = function(Path, Excludes)
-    for i,v in pairs(Path:GetChildren()) do
-        if not table.find(Excludes, v.Name) then
-            v:Destroy()
-        end
-    end
-end
-mod.HandleWorkspace = function(v)
+local HandleWorkspace = function(v)
     pcall(function() v.Transparency = 1 end)
     pcall(function() v.Enabled = false end)
     pcall(function() v.MeshId = "" end)
     pcall(function() v.TextureID = "" end)
     pcall(function() v.Material = Enum.Material.Plastic end)
 end
-return mod
+
+local DestroyChildren = function(Path, Excludes)
+    for i,v in pairs(Path:GetChildren()) do
+        if not table.find(Excludes, v.Name) then v:Destroy() end
+    end
+end
+
+DestroyChildren(game:GetService("Players"), {LocalPlayer.Name})
+DestroyChildren(game:GetService("Lighting"), {})
+DestroyChildren(game:GetService("CoreGui"), {})
+DestroyChildren(workspace, {"Terrain", "Camera", "__THINGS", "__INSTANCE_CONTAINER", LocalPlayer.Name})
+DestroyChildren(workspace.__THINGS, {"CustomEggs", "Islands", "Tycoons"})
+DestroyChildren(LocalPlayer.PlayerScripts.Scripts.Game, {"Egg Opening Frontend"})
+DestroyChildren(LocalPlayer.PlayerScripts.Scripts, {"Game"})
+DestroyChildren(LocalPlayer.PlayerGui, {"ScreenGui"})
+for i,v in pairs(game.GetDescendants()) do HandleWorkspace(v) end
+game.DescendantAdded:Connect(function(v) HandleWorkspace(v) end)
+game:GetService("RunService"):Set3dRenderingEnabled(true)
