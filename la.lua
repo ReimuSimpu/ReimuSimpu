3e770332fae4c3c151c82c55171799cd305c6196
LordHippo <146130238+ReimuSimpu@users.noreply.github.com>
2025-02-10 08:48:19 -0600

    Update la.lua

--- la.lua
+++ la.lua
@@ -1,4 +1,26 @@
 local LocalPlayer = game.Players.LocalPlayer
+local HumanoidRootPart = LocalPlayer.Character:WaitForChild("HumanoidRootPart")
+local Humanoid = LocalPlayer.Character:WaitForChild("Humanoid")
+
+local BodyGyro = Instance.new("BodyGyro")
+local BodyVelocity = Instance.new("BodyVelocity")
+
+BodyGyro.P = 9e4
+BodyGyro.maxTorque = Vector3.new(9e9, 9e9, 9e9)
+BodyGyro.cframe = HumanoidRootPart.CFrame
+BodyGyro.Parent = HumanoidRootPart
+
+BodyVelocity.velocity = Vector3.new(0, 0, 0)
+BodyVelocity.maxForce = Vector3.new(9e9, 9e9, 9e9)
+BodyVelocity.Parent = HumanoidRootPart
+
+task.spawn(function()
+    while task.wait() do
+        Humanoid.PlatformStand = true
+        BodyVelocity.velocity = Vector3.new(0, 0.1, 0)
+        BodyGyro.cframe = HumanoidRootPart.CFrame
+    end
+end)
 
 local HandleWorkspace = function(v)
     pcall(function() v.Transparency = 1 end)
